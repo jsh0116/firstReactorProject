b56b97c66d611a167e29bf0cdb2984f5ec6c252d
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-04-15 17:20:59 +0900

    Initial commit

---
+++ src/main/java/com/bithumb/firstReactorProject/FirstReactorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bithumb.firstReactorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstReactorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstReactorProjectApplication.class, args);
+	}
+
+}
